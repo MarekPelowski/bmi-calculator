cd39ca28dd7a37082f078f46454b1b2086b510eb
Marek Pelowski <marek.pelowski@protonmail.com>
2023-04-29 08:41:17 +0200

    created a calculation button and 2 reminders

--- main.py
+++ main.py
@@ -14,12 +14,21 @@ weight_label.place(x=20, y=30)
 weight_entry = Entry(root, font=("consolas", 18))
 weight_entry.place(x=110, y=30)
 
+weight_reminder = Label(root, text="kilograms", font=("consolas", 18, "italic"), bg="lightblue")
+weight_reminder.place(x=375, y=30)
+
 
 height_label = Label(root, text="height", font=("consolas", 18), bg="lightblue")
 height_label.place(x=20, y=80)
 
 height_entry = Entry(root, font=("consolas", 18))
 height_entry.place(x=110, y=80)
+
+height_reminder = Label(root, text="meters", font=("consolas", 18, "italic"), bg="lightblue")
+height_reminder.place(x=375, y=80)
+
+calculation_button = Button(root, text="calculate", font=("consolas", 15), bg="white")
+calculation_button.place(x=20, y=140)
 
 
 root.mainloop()
