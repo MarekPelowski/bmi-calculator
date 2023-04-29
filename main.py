b5e819e061a237dd87fe959910e1552047c30cf6
Marek Pelowski <marek.pelowski@protonmail.com>
2023-04-29 08:26:10 +0200

    created my first window :)

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("test")
+from tkinter import *
+
+root = Tk()
+root.geometry("500x500")
+root.resizable(0, 0)
+root.configure(bg="lightblue")
+root.title("bmi calculator")
+root.iconbitmap("questhead")
+
+
+root.mainloop()
