2dbfce6b7f193375474b0f2675a6e6d7d00a85ab
Marek Pelowski <marek.pelowski@protonmail.com>
2023-04-29 08:33:17 +0200

    created another button

--- main.py
+++ main.py
@@ -14,4 +14,12 @@ weight_label.place(x=20, y=30)
 weight_entry = Entry(root, font=("consolas", 18))
 weight_entry.place(x=110, y=30)
 
+
+height_label = Label(root, text="height", font=("consolas", 18), bg="lightblue")
+height_label.place(x=20, y=80)
+
+height_entry = Entry(root, font=("consolas", 18))
+height_entry.place(x=110, y=80)
+
+
 root.mainloop()
